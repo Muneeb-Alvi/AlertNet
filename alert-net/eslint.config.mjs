601ebcb464f1e59b238081e3fd02654a71071e04
Muneeb Alvi <maa9509@nyu.edu>
2025-02-16 22:16:12 +0400

    stuff

--- alert-net/eslint.config.mjs
+++ alert-net/eslint.config.mjs
@@ -10,10 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals"),
   {
     rules: {
-      "@typescript-eslint/*": "off", // This disables all TypeScript rules
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/ban-types": "off",
     },
   },
 ];
